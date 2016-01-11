95ec81463b441074f587009480bdc75721a37554
Eric Levine <eric.levine@apx-labs.com>
2016-01-11 15:45:16 -0500

    Switching to a custom built nuget package of Castle for Android

--- UnitTests.Android/MainActivity.cs
+++ UnitTests.Android/MainActivity.cs
@@ -22,11 +22,11 @@ namespace Moq.Tests
             AddTestAssembly(Assembly.GetExecutingAssembly());
 
             AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
-            // or in any reference assemblies			
+			// or in any reference assemblies			
 
-            //AddTestAssembly(typeof(PortableTests).Assembly);
-            // or in any assembly that you load (since JIT is available)
-
+			//AddTestAssembly(typeof(PortableTests).Assembly);
+			// or in any assembly that you load (since JIT is available)
+			AutoStart = true;
 #if false
 			// you can use the default or set your own custom writer (e.g. save to web site and tweet it ;-)
 			Writer = new TcpTextWriter ("10.0.1.2", 16384);
@@ -35,8 +35,8 @@ namespace Moq.Tests
 			// crash the application (to ensure it's ended) and return to springboard
 			TerminateAfterExecution = true;
 #endif
-            // you cannot add more assemblies once calling base
-            base.OnCreate(bundle);
+			// you cannot add more assemblies once calling base
+			base.OnCreate(bundle);
         }
     }
 }
